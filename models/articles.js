a2c8e40a4d2c5de6f34e0191006935048bc21282
Yusuf <holla2specialyusuf@gmailcom>
2023-12-21 09:37:23 +0100

    working api

---
+++ models/articles.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const articleSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+module.exports = mongoose.model('Article', articleSchema, 'articles')
